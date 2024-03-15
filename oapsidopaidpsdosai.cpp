04142ba3ef46a78998927eb0fea75e30af2d1b45
akiyamakai <grusha.cs@gmail.com>
2024-03-15 09:23:37 +0200

    Добавьте файлы проекта.

---
+++ oapsidopaidpsdosai.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+    const int SIZE = 10; 
+    int arr[SIZE] = { 1, 2, 3, 2, 4, 5, 1, 6, 4, 4 }; 
+
+    std::unordered_map<int, int> frequencyMap;
+    std::vector<int> duplicates; 
+
+   
+    for (int i = 0; i < SIZE; ++i) {
+        frequencyMap[arr[i]]++;
+    }
+
+    
+    for (const auto& pair : frequencyMap) {
+        if (pair.second > 1) {
+            duplicates.push_back(pair.first);
+        }
+    }
+
+   
+    if (duplicates.empty()) {
+        std::cout << "There are no duplicate elements in the array." << std::endl;
+    }
+    else {
+        std::cout << "Duplicate elements in an array: ";
+        for (int duplicate : duplicates) {
+            std::cout << duplicate << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
